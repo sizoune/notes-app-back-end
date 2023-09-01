29fdccac7e17df3c3a2459a54156cf4f6308933f
muhammad wildan iskandar <muhammadwildaniskandar@gmail.com>
2023-09-01 19:21:16 +0800

    first commit

---
+++ src/server.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+
+const Hapi = require('@hapi/hapi');
+const notes = require('./api/notes');
+const NotesService = require('./services/inMemory/NotesServices');
+const NotesValidator = require('./validator/notes');
+const { Client } = require('pg');
+
+const init = async () => {
+  const notesService = new NotesService();  
+  
+  const server = Hapi.server({
+    port: process.env.PORT,
+    host: process.env.HOST,
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+  
+  await server.register({
+    plugin: notes,
+    options: {
+      service: notesService,
+      validator: NotesValidator,
+    },
+  });
+  
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
